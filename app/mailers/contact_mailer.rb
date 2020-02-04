dab2d270d8c0f1ee07b540e2f2ab7bf825134634
ratwoods <programratje@gmail.com>
2020-02-04 21:08:21 +0100

    test mailer

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,12 @@
+class ContactMailer < ActionMailer::Base
+ default to: "programratje@gmail.com"
+
+ def contact_email(name, email, message)
+   @name = name
+   @email = email
+   @message = message
+
+ mail(from: email, subject: 'PatArt.com Contact Form Message')
+ end
+end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_03_140330) do
+ActiveRecord::Schema.define(version: 2020_02_04_183740) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 2020_02_03_140330) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.text "message"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
