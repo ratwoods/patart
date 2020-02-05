b5a893a77b2c333eeecd7fbe9b32ac2efa730fb1
ratwoods <programratje@gmail.com>
2020-02-05 23:32:59 +0100

    set up email

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,11 +2,11 @@ class ContactMailer < ActionMailer::Base
  default to: "programratje@gmail.com"
 
  def contact_email(name, email, message)
-   @name = name
-   @email = email
-   @message = message
-
+  @name = name
+  @email = email
+  @message = message
  mail(from: email, subject: 'PatArt.com Contact Form Message')
  end
 end
+
 
