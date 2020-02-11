a443781465945d00256d91a80275f482281a903a
ratwoods <programratje@gmail.com>
2020-02-11 17:12:09 +0100

    added a lot off stuff

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   root to: 'pages#index'
   get 'pages/contact'
   get 'pages/about'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
--- db/migrate/20200207125038_create_comments.rb
+++ db/migrate/20200207125038_create_comments.rb
@@ -2,10 +2,10 @@ class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
       t.text :body
-      t.references :user, null: false, foreign_key: true
+      t.references :user, null: true, foreign_key: true
       t.references :article, null: false, foreign_key: true
 
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
